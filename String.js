788bab9ad8e13872ce1e444c1ebb5cdb2b9a1337
Pratik Awasthi <awasthipratik.98@gmail.com>
2022-01-08 01:00:24 +0530

    Create String.js

---
+++ String.js
@@ -0,0 +1,11 @@
+    htl="orange apple";
+console.log(htl.indexOf('a'));
+console.log(htl.charAt(2));
+console.log(htl.includes('range'));
+console.log(htl.endsWith('e'));
+console.log(htl.substring(1,3));
+console.log(htl.slice(-4));
+console.log(htl.split(' '));
+console.log(htl.replace('a','o'));
+
+let html =`hello "persons" ${htl}`;
