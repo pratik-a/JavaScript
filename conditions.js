82da39ff8e6383499d94f4de7fff3b441548b79a
Pratik Awasthi <awasthipratik.98@gmail.com>
2022-01-08 11:43:17 +0530

    Create conditions.js

---
+++ conditions.js
@@ -0,0 +1,62 @@
+age=25;
+if(age<20)
+{
+    console.log("Student");
+}
+else if(age<=40 && age>=20)
+{
+    console.log("office");
+}
+else
+{
+    console.log("read");
+}
+
+if(age==='20')
+{
+    console.log("same value and type")
+}
+
+if(typeof vari!=='undefined')
+{
+    console.log("defined");
+}
+else
+{
+    console.log("not defined");
+}
+console.log(age>20?'walk':'run');
+
+/*console.log("switch");
+age=30;
+switch(age)
+{
+    case age<20:console.log("Student");
+        break;
+    case age>=20:console.log("office");
+        break;
+    default:console.log("read");
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
