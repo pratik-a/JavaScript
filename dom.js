f0de832400c3076a959cf0bdc7728837b65ff87c
Pratik Awasthi <awasthipratik.98@gmail.com>
2022-01-09 13:42:46 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,23 @@
+let a = window;
+aa = window.innerHeight;
+ab = window.innerWidth;
+console.log(aa);
+console.log(ab);
+a = scrollX;
+console.log(a);
+a = scrollY;
+console.log(a);
+a = location;
+console.log(a);
+a = location.toString();
+console.log(a);
+a = window.history;
+console.log(a);
+alert('hello user');
+
+
+b = prompt('type your name');
+console.log(b);
+
+c = confirm("are you sure ?");
+console.log(c);
